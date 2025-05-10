9a850f0d743f9a14b1437bd70928b25e628c1856
Emmanuel Calistus <99438779+Ecotech5@users.noreply.github.com>
2025-05-10 12:30:05 -0700

    Resolve merge conflict

--- myApp.js
+++ myApp.js
@@ -1,61 +1,24 @@
 const express = require('express');
 const app = express();
 
+// ✅ Step 1: Require helmet
+const helmet = require('helmet');
 
+// ✅ Step 2: Use helmet.hidePoweredBy()
+app.use(helmet.hidePoweredBy());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
+
+module.exports = app;
