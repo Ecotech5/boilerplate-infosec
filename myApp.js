6ed579146335e3db078954541aa832df8dbd55db
Ecotech5 <Emmanuelcailstus5@gmail.com>
2025-06-12 08:55:01 -0700

    Updated myApp.js and server.js with new logic

--- myApp.js
+++ myApp.js
@@ -1,30 +1,67 @@
 const express = require('express');
-const helmet = require('helmet');
+const Helmet = require('Helmet');
 const app = express();
 
-// ✅ Helmet security middlewares
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: 'deny' })); // ✅ Solves the clickjacking challenge
+const PORT= process.env.PORT|| 3030;
+app.listen(PORT, () =>{
+  console.log(`Ecotech is listening on port ${PORT}`);
+});
 
-// ✅ Public folder
-app.use(express.static('public'));
 
-// ✅ Disable HTTP strict transport security
-app.disable('strict-transport-security');
 
-// ✅ API route
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
 const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
 app.use('/_api', api);
-
-// ✅ Root route
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-
-// ✅ Port listen
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
-
-module.exports = app;
